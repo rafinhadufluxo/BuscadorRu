46d0f109c22e9071eeb1d563c742a8d3064a19d1
mazarafa7 <arruda.rafa.ra@gmail.com>
2020-05-24 15:38:26 -0500

    arrumado os valores, falta deixar em matriz

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const request = require ('request-promise');
-const cheerio = require ('cheerio');
+const request = require('request-promise');
+const cheerio = require('cheerio');
 const Crawler = require('crawler');
 
 
 const crawpgFilha = new Crawler({
-    rateLimit:5000,
-    callback: function(error,res, done){
-        if(error){
+    rateLimit: 5000,
+    callback: function (error, res, done) {
+        if (error) {
             Console.log(error)
-        }else{
+        } else {
+
             let $ = res.$;
             let nome = $('#content-core > div > span > dl:nth-child(13) > dd > table > tbody').text().trim()
             console.log(nome);
@@ -16,4 +17,4 @@ const crawpgFilha = new Crawler({
     }
 })
 
-crawpgFilha('https://www.uffs.edu.br/campi/chapeco/restaurante_universitario')
+crawpgFilha.queue('https://www.uffs.edu.br/campi/chapeco/restaurante_universitario')
